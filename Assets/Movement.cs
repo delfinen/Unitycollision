e9fca29f8b07584e658268dd3b6c601021d5342d
LEARNET\daniel.aleksovski <daniel.aleksovski@elev.ga.lbs.se>
2018-10-16 10:45:37 +0200

    maze med veggar som dödar

---
+++ Assets/Movement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour {
+    public Rigidbody2D rbody;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+     
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+
+    }
+
+
+    //Lägg allt som har med fysik att göra
+    private void FixedUpdate()
+    {
+       
+
+
+
+            if (Input.GetKey(KeyCode.A))
+        {
+            rbody.AddForce(new Vector2(-10f, 0f));
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rbody.AddForce(new Vector2(10f, 0f));
+        }
+
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rbody.AddForce(new Vector2(0f, 10f));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rbody.AddForce(new Vector2(0f, -10f));
+        }
+
+        
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
