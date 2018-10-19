915b94c2c60d5e9d177f20f599327dd76de15900
LEARNET\daniel.aleksovski <daniel.aleksovski@elev.ga.lbs.se>
2018-10-19 15:38:59 +0200

    animeation

---
+++ Assets/animationscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationscript : MonoBehaviour {
+
+    public Animator anim;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("ha", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            anim.SetBool("ha", false);
+        }
+
+
+
+    }
+}
